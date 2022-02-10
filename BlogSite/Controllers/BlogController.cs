028d4c866f07cc2f24c5f56d4ac50d59ea6679e4
serdooooo <karakurtserdar19@gmail.com>
2022-02-10 05:35:11 +0300

    Add project files.

---
+++ BlogSite/Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogSite.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
